90924f21ea5e1d7decc507670b245d4a9d2cb2b3
dnjsgur0629 <sisun_@naver.com>
2021-12-05 17:36:08 +0900

    feat (MainActivity.java):SG6-52 SG6-75 #comment barcode_scan via Gallery

---
+++ app/src/main/java/com/SE_project/barcode_scanner/NaverApiActivity.java
@@ -0,0 +1,65 @@
+package com.SE_project.barcode_scanner;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.os.StrictMode;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class NaverApiActivity extends AppCompatActivity {
+    NaverApiHelper naver_api_helper;
+    public static StringBuilder strBuilder; //
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.naver_api_test);
+        System.out.println("NaverApiActivity 테스트");
+        Log.d("request: ","NaverApiActivity 테스트");
+        naver_api_helper = new NaverApiHelper();
+        System.out.println("Url 기져오기" + naver_api_helper.getBasicUrl());
+        try {
+            System.out.println("try안");
+            String productName = URLEncoder.encode("롯데 빠다코코낫 78G", "utf-8");
+            String apiURL = naver_api_helper.getBasicUrl() +
+                    naver_api_helper.getQuery() + productName +
+                    naver_api_helper.getDisplay() +
+                    naver_api_helper.getStart() +
+                    naver_api_helper.getSort();
+            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().permitNetwork().build());
+
+            URL url = new URL(apiURL);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("X-Naver-Client-Id", naver_api_helper.getClientId());
+            con.setRequestProperty("X-Naver-Client-Secret", naver_api_helper.getClientSecret());
+            int responseCode = con.getResponseCode();
+            BufferedReader br;
+            if (responseCode == 200) {
+                br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            } else {
+                br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+            }
+            strBuilder = new StringBuilder();
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                strBuilder.append(line + "\n");
+                Log.d("request: ",line);
+            }
+            br.close();
+            con.disconnect();
+            System.out.println(strBuilder);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+}
---
+++ app/src/main/java/com/SE_project/barcode_scanner/NaverApiHelper.java
@@ -0,0 +1,59 @@
+package com.SE_project.barcode_scanner;
+
+public class NaverApiHelper {
+    NaverShoppingApiToken naver_api_token;  //NaverApi clientId,clientSecret key들을 불러오기위함
+    protected String clientId;              //X-Naver-Client-Id
+    protected String clientSecret;          //X-Naver-Client-Secret
+    protected final static String basicUrl = "https://openapi.naver.com/v1/search/shop.json?";
+    protected String query = "query=";      // 필수여부(Y)
+    protected String display = "&display=2"; // 필수여부(N) 기본값(10) 최대값(100)
+    protected String start = "&start=1";     // 필수여부(N) 기본값(1) 최대값(1000)
+    protected String sort = "&sort=asc";    // 필수여부(N) 기본값(sim) , date , asc , dsc
+    protected String fullUrl;               //Get 방식으로 Request를 보낼 Url
+
+    NaverApiHelper() {
+        naver_api_token = new NaverShoppingApiToken();          //NaverApi Key들을 불러오기위한 생성자 생성
+        this.clientId=naver_api_token.getClientId();            //Secret file인 NaverShoppingApiToken에서 key값을 가져와 API Key를 github에 가린다.
+        this.clientSecret=naver_api_token.getClientSecret();    //Secret file인 NaverShoppingApiToken에서 key값을 가져와 API Key를 github에 가린다.
+    }
+
+    /* set Method => query:상품이름 , fullUrl:request 보내기 위한 최종 Url */
+    protected void setFullUrl(String fullUrl) {
+        this.fullUrl = fullUrl; //request 보내는 Url
+    }
+    protected void setQuery(String query) {
+        this.query = query; //상품이름
+    }
+
+    /* get Method */
+    protected String getClientId() {
+        return clientId;
+    }
+    protected String getClientSecret() {
+        return clientSecret;
+    }
+
+    protected String getQuery() {
+        return query;
+    }
+
+    protected String getDisplay() {
+        return display;
+    }
+
+    protected String getStart() {
+        return start;
+    }
+
+    protected String getSort() {
+        return sort;
+    }
+    protected String getBasicUrl() {
+        return basicUrl;
+    }
+
+    protected String getFullUrl() {
+        return fullUrl;
+    }
+
+}
