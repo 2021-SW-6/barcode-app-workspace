9dcb241007dd82450558288140a103883e894435
njh <nojahoon@naver.com>
2021-12-17 01:37:34 +0900

    feat(NaverApiActivity.java): SG6-85 #comment subtract sort url param for product_name accuracy(naver_shopping_api) and added json sorting code

--- app/src/main/java/com/SE_project/barcode_scanner/NaverApiActivity.java
+++ app/src/main/java/com/SE_project/barcode_scanner/NaverApiActivity.java
@@ -28,6 +28,10 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public class NaverApiActivity extends AppCompatActivity {
     NaverApiHelper naver_api_helper;
@@ -40,17 +44,15 @@ public class NaverApiActivity extends AppCompatActivity {
         Log.d("request: ","NaverApiActivity 테스트");
         naver_api_helper = new NaverApiHelper();
         System.out.println("Url 기져오기" + naver_api_helper.getBasicUrl());
-
-        JSONArray getArray;
-
+        //sort parameter주면 상품명에 대한 정확도가 떨어지는 것 확인=>안드로이드단에서 정렬하도록 수정
         try {
             System.out.println("try안");
             String productName = URLEncoder.encode("롯데 빠다코코낫 78G", "utf-8");
             String apiURL = naver_api_helper.getBasicUrl() +
                     naver_api_helper.getQuery() + productName +
                     naver_api_helper.getDisplay() +
-                    naver_api_helper.getStart() +
-                    naver_api_helper.getSort();
+                    naver_api_helper.getStart();
+                    //naver_api_helper.getSort();
             StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().permitNetwork().build());
 
             URL url = new URL(apiURL);
@@ -74,11 +76,38 @@ public class NaverApiActivity extends AppCompatActivity {
             br.close();
             con.disconnect();
             System.out.println(strBuilder);
-
+            /* ----- 읽어온 내용 Json화 -----*/
             JSONObject jsonObject = new JSONObject(strBuilder.toString());
-            getArray = (JSONArray) jsonObject.get("items");
+            JSONArray getArray = (JSONArray) jsonObject.get("items");
+            /* 가격으로 Sort (Parameter로 sort asc를 하면 상품명의 정확도가 떨어지게 되므로
+            따로 정렬처리하기위함*/
+            JSONArray sortedJsonArray = new JSONArray();
+            List<JSONObject> jsonValues = new ArrayList<JSONObject>();
+            for(int i=0; i<getArray.length(); i++) {
+                jsonValues.add(getArray.getJSONObject(i));
+            }
+            Collections.sort(jsonValues, new Comparator<JSONObject>() {
+                private static final String KEY_NAME = "lprice";
+
+                @Override
+                public int compare(JSONObject a, JSONObject b) {
+                    Integer valA = 0;
+                    Integer valB = 0;
+                    try{
+                        valA = Integer.parseInt((String)(a.get(KEY_NAME)));
+                        valB = Integer.parseInt((String)(b.get(KEY_NAME)));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    return valA.compareTo(valB);
+                }
+            });
+            for(int i=0; i<getArray.length(); i++) {
+                sortedJsonArray.put(jsonValues.get(i));
+            }
+            /* Sorted된 sortedJsonArray를 TableRow로 추가해줌.*/
             for (int i = 0; i < getArray.length(); i++) {
-                JSONObject object = (JSONObject) getArray.get(i);
+                JSONObject object = (JSONObject) sortedJsonArray.get(i);
                 addTableRow(object ,this);
             }
         } catch (Exception e) {
@@ -91,10 +120,13 @@ public class NaverApiActivity extends AppCompatActivity {
         String getTitle = (String) jsonObject.get("title");
         String getPrice = (String) jsonObject.get("lprice");
         String getLink = (String) jsonObject.get("link");
-        String getMall = (String) jsonObject.get("mallName");
 
         String titleFilter = getTitle.replaceAll("<b>", "");
         String title = titleFilter.replaceAll("</b>", "");
+        if(title.length()>25) {
+            String strnew = title.substring(0,25);
+            title = strnew.trim() + "...";
+        }
 
         TableLayout tableLayout = (TableLayout)findViewById(R.id.myTableLayout);
 
