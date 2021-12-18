c1145842f9ef8fac61bfd3650cf383d35eaa5d15
dnjsgur0629 <sisun_@naver.com>
2021-12-18 20:12:11 +0900

    fix (Eliminate code conflicts)

--- app/src/main/java/com/SE_project/barcode_scanner/GoodsInfoActivity.java
+++ app/src/main/java/com/SE_project/barcode_scanner/GoodsInfoActivity.java
@@ -30,67 +30,17 @@ import java.net.URLEncoder;
 public class GoodsInfoActivity extends AppCompatActivity {
     TextView tvBarcodeNumber; //인식한 내용 출력되는 것 확인
     TextView tvProductName;   //상품명 나중에 정보읽어와서 넣어주어야하는부분
-<<<<<<< HEAD
-    TextView tv_lowest_price;   //최저가 나중에 정보읽어와서 넣어주어야하는부분
-=======
->>>>>>> c26b5cd92f9de0963d0981ca871e8e1d0a137834
-    //NaverApiHelper naver_api_helper;          =>기능개발 완료되면 customize
-    //public static StringBuilder strBuilder;   =>기능개발 완료되면 customize
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_goods_info);
         setTitle("상품 정보");
-        Log.v("onCreate","onCreate func");
 
         tvBarcodeNumber = findViewById(R.id.tvBarcodeNumber);   //텍스트뷰 연결
         tvProductName = findViewById(R.id.tvProductName);
 
         Intent intent = getIntent();
-<<<<<<< HEAD
-        String barcodeNumber = intent.getStringExtra("barcodeNumber");
-        tvBarcodeNumber = findViewById(R.id.tvBarcodeNumber);
-        tvBarcodeNumber.setText(barcodeNumber);
-        String URL = "http://www.koreannet.or.kr/home/hpisSrchGtin.gs1?gtin=";
-        URL += barcodeNumber;
-        final String requestURL =  URL;
-        new Thread() {
-            @Override
-            public void run() {
-                Log.v("Thread run() " , "Thread run함수안");
-                Bundle bundle = new Bundle();
-                boolean isEmpty;
-                try {
-                    Log.v("Try " , "try 구문시작");
-                    Document doc = Jsoup.connect(requestURL).get();
-                    Log.v("Document doc 아래", "Document doc 아래");
-                    Elements prodInfo = doc.select(".productTit");
-                    Log.v("Elements prodInfo 아래", "Elements prodInfo 아래");
-                    isEmpty = prodInfo.isEmpty(); //빼온 값 null체크
-                    Log.d("Tag", "isNull? : " + isEmpty); //로그캣 출력
-
-                    if(isEmpty == false) { //null값이 아니면 크롤링 실행
-                        //String prodName = prodInfo.get(15).text().substring(0); //크롤링 하면 "현재온도30'c" 이런식으로 뽑아와지기 때문에, 현재온도를 잘라내고 30'c만 뽑아내는 코드
-                        bundle.putString("prodName", prodInfo.toString()); //prodName에 prodInfo.toString() 찍어줌=> 8801062522064 &nbsp;&nbsp;&nbsp;&nbsp; 롯데 빠다코코낫 78G
-                        Log.v("prodInfo.toString()" ,prodInfo.toString());
-
-                    }
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }.start();
-
-        Handler handler = new Handler(Looper.getMainLooper()) {
-            @Override
-            public void handleMessage(@NonNull Message msg) {
-                Bundle bundle = msg.getData();    //new Thread에서 작업한 결과물 받기
-                tvProductName = findViewById(R.id.tvProductName);
-                tvProductName.setText(bundle.getString("prodName"));    //받아온 데이터 textView에 출력
-            };
-        };
-=======
         String barcodeNumber = intent.getStringExtra("barcodeNumber");  //MainActivity에서 barcodeNumber 전달받음
         tvBarcodeNumber.setText("바코드 번호 : " + barcodeNumber);
 
@@ -147,6 +97,5 @@ public class GoodsInfoActivity extends AppCompatActivity {
                 }
             }
         }.start();
->>>>>>> c26b5cd92f9de0963d0981ca871e8e1d0a137834
     }
 }
